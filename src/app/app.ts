fe6b5a762d8e4999c92e06b723aa4ac835fd4476
sace filmes <sacefilmes@Mac-Studio-de-sace.local>
2025-08-07 22:10:29 -0300

    first commit component and settings

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,19 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { Navbar } from "./_components/navbar/navbar";
+import { CommonModule } from '@angular/common';
+import { PrimaryButton } from './_components/primary-button/primary-button';
+import { SecondaryButton } from './_components/secondary-button/secondary-button';
+import { ItemCertificado } from './_components/item-certificado/item-certificado';
+import { BaseUi } from './_components/base-ui/base-ui';
+import { Certificados } from './pages/certificados/certificados';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Navbar, CommonModule, BaseUi, Certificados],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('gerador-certificado');
+  title = 'gerador-certificado';
+  exibeNavbar: boolean = true;
 }
