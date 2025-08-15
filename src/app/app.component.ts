c2fb60dcf5a512a264479289e5a3743de3d3312d
guipardindev <guipardindev@gmail.com>
2025-08-14 23:48:41 -0300

    Retrieving certificate information on pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavbarComponent } from "./_components/navbar/navbar.component";
 import { CommonModule } from '@angular/common';
 import { PrimaryButtonComponent } from './_components/primary-button/primary-button.component';
@@ -6,6 +6,7 @@ import { SecondaryButtonComponent } from './_components/secondary-button/seconda
 import { ItemCertificadoComponent } from './_components/item-certificado/item-certificado.component';
 import { BaseUiComponent } from './_components/base-ui/base-ui.component';
 import { RouterOutlet } from '@angular/router';
+import { CertificadoService } from './_services/certificado.service';
 
 
 @Component({
@@ -14,7 +15,15 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'gerador-certificado';
   exibeNavbar: boolean = true;
+
+  constructor(private certificadoService: CertificadoService) {}
+
+  ngOnInit(): void {
+    const certificados = localStorage.getItem('certificados');
+    this.certificadoService.certificados = certificados ? JSON.parse(certificados) : [];
+    console.log(this.certificadoService.certificados);
+  }
 }
