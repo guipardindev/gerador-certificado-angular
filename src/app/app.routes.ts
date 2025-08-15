ca9bb433691bd9f8847c79869695d39c5e14d9d3
guipardindev <guipardindev@gmail.com>
2025-08-14 23:35:27 -0300

    refactored code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,19 @@
 import { Routes } from '@angular/router';
-import { Certificados } from './pages/certificados/certificados';
-import { CertificadoForm } from './pages/certificado-form/certificado-form';
-import { Certificado } from './pages/certificado/certificado';
+import { CertificadosComponent } from './pages/certificados/certificados.component';
+import { CertificadoFormComponent } from './pages/certificado-form/certificado-form.component';
+import { CertificadoComponent } from './pages/certificado/certificado.component';
 
 export const routes: Routes = [
   {
     path: "",
-    component: Certificados,
+    component: CertificadosComponent,
   },
   {
     path: "certificados/novo",
-    component: CertificadoForm,
+    component: CertificadoFormComponent,
   },
   {
     path: "certificados/:id",
-    component: Certificado,
+    component: CertificadoComponent,
   },
 ];
